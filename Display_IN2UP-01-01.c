f1f61241ea72ee0a1181ee3b8f8044a0d4ac61c8
chals24x <84355471+ChalsLapWork@users.noreply.github.com>
2024-10-18 13:08:01 -0600

    create head
    creacion del proyecto

---
+++ Display_IN2UP-01-01.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <wiringPi.h>
+//LED PIN WIRINpi 0 is bcm_gpio 17.
+#define BIT0 0
+#define BIT1 1
+#define BIT2 2
+#define BIT3 3
+#define BIT4 4
+#define BIT5 5
+#define BIT6 6
+#define BIT7 7
+#define WR   8
+#define BUSY 9
+
+int main(void){
+  printf("\nInizializacion de Display");
+  wiringPiSetup();
+  pinMode(BIT0,OUTPUT);
+  pinMode(BIT1,OUTPUT);
+  pinMode(BIT2,OUTPUT);
+  pinMode(BIT3,OUTPUT);
+  pinMode(BIT4,OUTPUT);
+  pinMode(BIT5,OUTPUT);
+  pinMode(BIT6,OUTPUT);
+  pinMode(WR,OUTPUT);
+  pinMode(BUSY,OUTPUT);
+
+for(;;){
+   
+
+}//fin for++++++++++++++++++++
+
+
+
+}//fin de main+++++++++++++++++++++++++++++++++++++++
+
