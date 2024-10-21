790b87c916494c64b9c13aadb247b3564313f44f
chals24x <84355471+ChalsLapWork@users.noreply.github.com>
2024-10-21 16:06:12 -0600

    debugging 21-oct-24 4:06pm
    debugging

--- VFDisplay.c
+++ VFDisplay.c
@@ -7,11 +7,12 @@ extern struct _DISPLAY_VFD_ vfd;
 
 //metodo unico de padre unico  y sin instancias
 void Init_VFD( void){
-   
+unsigned char ret=0;   
   //if(recurso.solicitar(_VFD_,PID_xInit_VFD1)){
-  VFDcommand(0x1BU); // init display  ESC@= 1BH,40H
-  VFDcommand(0x40U);	 // Setting return to default values 
-  FontSizeVFD(FONTSIZE2);//TAMAÑO DE fuente 2->8x16
-  vfd.config.bits.init_VFD=TRUE;
-			  
+  ret+=VFDcommand(0x1BU); // init display  ESC@= 1BH,40H
+  ret+=VFDcommand(0x40U);	 // Setting return to default values 
+  ret+=FontSizeVFD(FONTSIZE2);//TAMAÑO DE fuente 2->8x16
+  if(ret==3)
+       vfd.config.bits.init_VFD=TRUE;
+  else errorCritico("\nNo se inizializa el display");	   	  
 }//fin init VFD -------------------------------------------------------------------
