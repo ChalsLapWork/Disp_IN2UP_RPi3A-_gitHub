df6420600bd87257807e5e94abe437354e36289f
chals24x <84355471+ChalsLapWork@users.noreply.github.com>
2024-10-29 08:48:07 -0600

    debug 29-oct-24 847am
    DEBUG
    func basicas de init: finish
    vfd transmit motor: building 71	%
    display init:building 44%
    display menu:no yet

--- queue.c
+++ queue.c
@@ -127,7 +127,7 @@ return TRUE;
 void* SubProceso_Tx_VFD(void* arg) {
     FIFO_VFD* q = (FIFO_VFD*)arg;
 	struct VFD_DATA data;
-	unsigned char estado124;
+	unsigned char estado124,ret=0;
 	printf("\n       Proceso  Transmissor a VFD Iniziando");
 	while(!ret){
 	 switch(estado124){
