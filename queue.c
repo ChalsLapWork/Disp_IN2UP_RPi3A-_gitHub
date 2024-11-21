0eba9b3ec2172538c8ff1f1accae2a5095bd8d98
chals24x <84355471+ChalsLapWork@users.noreply.github.com>
2024-11-21 15:32:28 -0600

    debug 21-nov.24 332pm
    debug
    Hilo VFD tx:85%
    Hilo VFD init:85%
    function write parallel:64%

--- queue.c
+++ queue.c
@@ -204,7 +204,7 @@ const unsigned char SIZE_CMD=7;//numero de comandos
 const unsigned char s[7]={0x1BU,0x40U,0x1FU,0x28U,0x67U,0x01U,FONTSIZE2};
 unsigned char i=0;
 */
-printf("\n       Init VFD");
+printf("\n       Init VFD running");
   
 /*  if(q->v->config.bits.init_VFD){
 	   errorCritico("ya esta inizializado Proceso, Error de duplicacion");}	   	   
